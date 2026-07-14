2969ac4c302f68cf1d230d95b8eab6a0a3fd126d
RISHABH KUMAR SINGH <rishabhsingh8989r@gmail.com>
2026-07-14 12:57:03 +0530

    Student Details

---
+++ src/main/java/com/example/demo/studentServer/Student.java
@@ -0,0 +1,33 @@
+package com.example.demo.studentServer;
+
+public class Student {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeparment() {
+        return deparment;
+    }
+
+    public void setDeparment(String deparment) {
+        this.deparment = deparment;
+    }
+
+    int id;
+    String name;
+    String deparment;
+
+
+}
---
+++ src/main/java/com/example/demo/studentServer/StudentServer.java
@@ -0,0 +1,31 @@
+package com.example.demo.studentServer;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentServer {
+
+//    1 Store the student
+
+    @PostMapping("/create")
+    public String storeStudent(@RequestBody Student student ){
+        String name=student.getName();
+        int id=student.getId();
+        String deparment=student.getDeparment();
+
+        return "id: " + id + " \n " +
+                "name: "+ name + " \n   " +
+                "deparment: "+ deparment;
+    }
+//    2 read the student with id
+//    3 Update the student information
+//    4 delete the student information
+//
+
+
+
+}
